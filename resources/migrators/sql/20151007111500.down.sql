c388083193c6bb081c0617726f36750796070ef4
Hildeberto Mendonca <me@hildeberto.com>
2015-10-09 14:14:16 +0200

    Fixed #11

---
+++ resources/migrators/sql/20151007111500.down.sql
@@ -0,0 +1,2 @@
+alter table milestone drop column start_sprint;
+alter table milestone drop column type;
---
+++ resources/migrators/sql/20151007111500.up.sql
@@ -0,0 +1,2 @@
+alter table milestone add start_sprint timestamp null;
+alter table milestone add type varchar(20) null;
